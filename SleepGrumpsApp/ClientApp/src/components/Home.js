a130e822683dd63770be1c98ca16757b9e8e1b2e
Aidan Baker <baker339@purdue.edu>
2021-05-09 12:12:34 -0400

    Added 3d effect to card

--- SleepGrumpsApp/ClientApp/src/components/Home.js
+++ SleepGrumpsApp/ClientApp/src/components/Home.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
+import Card from "reactstrap/lib/Card";
 import HomeTwo from "./HomeTwo";
 
 export class Home extends Component {
   static displayName = Home.name;
+
+  componentDidMount() {
+    // Movement Animation to Happen
+    const homeTwoContainer = document.querySelector(".home-two-container");
+
+    //Moving Animation Event
+    homeTwoContainer.addEventListener("mousemove", (e) => {
+      let xAxis = (window.innerWidth / 2 - e.pageX) / 300;
+      let yAxis = (window.innerHeight / 2 - e.pageY) / 300;
+
+      homeTwoContainer.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+    });
+
+    //Animate in
+    homeTwoContainer.addEventListener("mouseenter", (e) => {
+      homeTwoContainer.style.transition = "none";
+    });
+
+    //Animate out
+    homeTwoContainer.addEventListener("mouseleave", (e) => {
+      homeTwoContainer.style.transition = "all 0.5s ease";
+      homeTwoContainer.style.transform = `rotateY(0deg) rotateX(0deg)`;
+    });
+  }
 
   render() {
     return <HomeTwo />;
